b9a3e3fdd9b5d845077b58b6fd7778d0bfae0e2f
Jongkeun Kim <kjkandrea@gmail.com>
2020-10-02 16:09:27 +0900

    문제 풀이

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+      "eslint:recommended", "eslint-config-prettier"
+    ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
